856ca5243d58bec0f790f9fcb2c1933f0ecc3da5
David San <davidsanfr@gmail.com>
2023-03-14 17:32:23 +0100

    allow fwd on 443

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Allow all traffic from Cloudflare IPs (no ports restriction)
-for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip comment 'Cloudflare IP'; done
-
-ufw reload > /dev/null
+#for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw route allow proto tcp from $cfip comment 'Cloudflare IP'; done
 
 # OTHER EXAMPLE RULES
 # Retrict to port 80
-#for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip to any port 80 comment 'Cloudflare IP'; done
+#for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw route allow proto tcp from $cfip to any port 80 comment 'Cloudflare IP'; done
 
 # Restrict to port 443
-#for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip to any port 443 comment 'Cloudflare IP'; done
+for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw route allow proto tcp from $cfip to any port 443 comment 'Cloudflare IP'; done
 
 # Restrict to ports 80 & 443
-#for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare IP'; done
+#for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw route allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare IP'; done
+
+ufw reload > /dev/null
